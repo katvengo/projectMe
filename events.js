d51ac4bf078ed76ff86d1f5003da7143be3bd881
Kat Vengo <katvengo@gmail.com>
2019-05-17 22:48:31 -0700

    working

---
+++ events.js
@@ -0,0 +1,11 @@
+var Events = [{
+    host: "yoda",
+    eventName: "Yoda's Birthday Party",
+    location: "Dagobah",
+    private: true,
+    description: "Jedi's only"
+},
+
+];
+
+module.exports = Events;
--- server.js
+++ server.js
@@ -3,21 +3,13 @@ var path = require("path");
 
 var app = express();
 
-var PORT = 8081;
+var PORT = process.env.PORT || 8081;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-var events = [{
-        host: "yoda",
-        eventName: "Yoda's Birthday Party",
-        location: "Dagobah",
-        private: true,
-        description: "Jedi's only"
-    },
-
-];
+var events = require('./Events.js');
 
 // Basic route that sends the user first to the AJAX Page
 app.get("*", function (req, res) {
@@ -29,24 +21,27 @@ app.get("/events", function (req, res) {
     res.sendFile(path.join(__dirname, "public/events.html"));
 });
 
-
+// app.get("/events", function (req, res) {
+//     return res.json(newEvents);
+// });
 
 
 // Create New Events - takes in JSON input
 app.post("/events", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
-    var newEvents = req.body;
+    var newEvent = req.body;
 
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newEvents.events = newEvents.eventName.replace(/\s+/g, "").toLowerCase();
+    newEvent.events = newEvent.eventName.replace(/\s+/g, "").toLowerCase();
 
-    console.log(newEvents);
+    console.log(newEvent);
 
-    events.push(newEvents);
+    events.push(newEvent);
 
-    res.json(newEvents);
+    res.json(newEvent);
+    alert('Event has posted successfully!')
 });
 
 // Starts the server to begin listening
